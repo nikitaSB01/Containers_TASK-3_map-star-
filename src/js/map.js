817391c9f31c959bd6e17cb8ff6c303f72192996
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-02-10 18:26:56 +0600

    доделал класс Settings

--- src/js/map.js
+++ src/js/map.js
@@ -1,8 +1,8 @@
 class Settings {
   constructor(...value) {
-    this.allThemeOptions = ['light', 'gray'];
-    this.allMusicOptions = ['pop', 'rock', 'chillout', 'off'];
-    this.allDifficultyOptions = ['normal', 'hard', 'nightmare'];
+    this.allThemeOptions = ['dark', 'light', 'gray'];
+    this.allMusicOptions = ['trance', 'pop', 'rock', 'chillout', 'off'];
+    this.allDifficultyOptions = ['easy', 'normal', 'hard', 'nightmare'];
     this.defaultMap = new Map();
     if (value) {
       value.forEach((item) => {
@@ -12,6 +12,17 @@ class Settings {
           this.defaultMap.set('music', item);
         } else if (this.allDifficultyOptions.includes(item)) {
           this.defaultMap.set('difficulty', item);
+        } else {
+          throw new Error(`Одно из введённых Вами значений не соответсвует доступным настройкам. 
+          Доступные настройки: 
+          theme: ['light', 'gray'] 
+          music: ['pop', 'rock', 'chillout', 'off']
+          difficulty: ['normal', 'hard', 'nightmare']
+          Настройки по умолчанию:
+          theme: ['dark'] 
+          music: ['trance']
+          difficulty: ['easy']
+          `);
         }
       });
     } else {
@@ -26,7 +37,7 @@ class Settings {
   }
 }
 
-const settin = new Settings('fefdsf', 'rock');
+const settin = new Settings('dark', 'rock');
 console.log(settin.getSet());
 
 //  export default Settings;
